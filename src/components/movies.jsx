c786c86624b6b00015b6a3881a8d9863555e5d62
KirannVaka <saikirann.vaka@gmail.com>
2022-09-21 18:15:01 +0800

    Code With Mosh - React - Components Excercise - Solution

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -11,43 +11,47 @@ function Movies() {
     setMovies(tempMovies);
   };
 
-  return (
-    <React.Fragment>
-      {movies.length > 0 && (
-        <h1> Showing {movies.length} movies in database.</h1>
-      )}
-      {movies.length === 0 && <h1>There are no movies in DB</h1>}
-      <table className="table">
-        <thead>
-          <tr>
-            <th scope="col">Title</th>
-            <th scope="col">Genre</th>
-            <th scope="col">Stock</th>
-            <th scope="col">Rate</th>
-          </tr>
-        </thead>
-        <tbody>
-          {movies.map((movie) => (
-            <tr key={movie.id + movie.title}>
-              <td key={movie.title}>{movie.title}</td>
-              <td key={movie.id + movie.genre}>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td key={movie.id}>{movie.dailyRentalRate}</td>
-              <td key={movie.id}>
-                <button
-                  key={movie._id}
-                  onClick={() => handleDelete(movie._id)}
-                  className="btn btn-danger"
-                >
-                  Delete
-                </button>
-              </td>
+  if (movies.length === 0) {
+    return <h1>There are no movies in DB</h1>;
+  } else {
+    return (
+      <React.Fragment>
+        {movies.length > 0 && (
+          <h1> Showing {movies.length} movies in database.</h1>
+        )}
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Title</th>
+              <th scope="col">Genre</th>
+              <th scope="col">Stock</th>
+              <th scope="col">Rate</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    </React.Fragment>
-  );
+          </thead>
+          <tbody>
+            {movies.map((movie) => (
+              <tr key={movie.id + movie.title}>
+                <td key={movie.title}>{movie.title}</td>
+                <td key={movie.id + movie.genre}>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td key={movie.id}>{movie.dailyRentalRate}</td>
+                <td key={movie.id}>
+                  <button
+                    key={movie._id}
+                    onClick={() => handleDelete(movie._id)}
+                    className="btn btn-danger"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </React.Fragment>
+    );
+  }
 }
 
 // class Movies extends Component {
